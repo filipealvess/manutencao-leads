bf0205d7b46d480c7a8811728ad96a16d271aec4
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-03-23 20:44:49 -0300

    feat: add main component

---
+++ src/components/Main/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const MainWrapper = styled.main`
+  width: 100%;
+  max-width: 1050px;
+  padding: 0 25px;
+  margin: 30px auto;
+`;
+
+export default MainWrapper;
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import Header from '../../components/Header';
+import Main from '../../components/Main';
+import Form from '../../components/Form';
+import Field from '../../components/Field';
 
 export default function Login() {
   return (
     <React.Fragment>
       <Header title="Manutenção de Leads" />
+
+      <Main>
+        <Form title="Entrar no sistema">
+          <Field label="Usuário" placeholder="Seu usuário" />
+          <Field label="Senha" placeholder="Sua senha" type="password" />
+          <Field label="Confirmação de senha" placeholder="Sua senha" type="password" />
+        </Form>
+      </Main>
     </React.Fragment>
   );
 }
