d4bbbccb39ff141ccd92c03990de804ba579588d
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-03-23 20:43:40 -0300

    feat: add field component

---
+++ src/components/Field/styles.js
@@ -0,0 +1,30 @@
+import styled, { css } from 'styled-components';
+
+export const FieldWrapper = styled.label`
+  display: flex;
+  flex-direction: column;
+
+  & + & { margin-top: 20px; }
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  padding: 5px 10px;
+  margin-top: 5px;
+  border: 1px solid ${({ theme }) => theme.inputBorderColor};
+  border-radius: 5px;
+  cursor: text;
+  transition: 0.2s border-color;
+
+  ${({ theme, isFocus }) => (
+    isFocus && css`border-color: ${theme.text};`
+  )}
+`;
+
+export const Input = styled.input`
+  flex-grow: 1;
+  min-width: 0;
+  margin: 5px 0;
+`;
+
+export default FieldWrapper;
