3f56517c1a3b05f07693807e86fe80ba4ebdaf71
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-03-23 17:14:20 -0300

    feat: add theme initial structure

---
+++ src/styles/Theme.js
@@ -0,0 +1,12 @@
+const theme = {
+  pageBackground: '#FFFFFF',
+  text: '#000000',
+  sectionBackground: '#F9F9F9',
+  sectionBorderColor: '#DEDEDE',
+  accent: '#5A60D0',
+  red: '#FE6262',
+  green: '#37CC3D',
+  lightBlue: '#C9DCF2',
+};
+
+export default theme;
