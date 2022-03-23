6a1597b024a76a3df237814b3f75d94ba183a47a
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-03-23 20:34:02 -0300

    chore(theme): add inputBorderColor

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -3,6 +3,7 @@ const theme = {
   text: '#000000',
   sectionBackground: '#F9F9F9',
   sectionBorderColor: '#DEDEDE',
+  inputBorderColor: '#AEAEAE',
   accent: '#5A60D0',
   red: '#FE6262',
   green: '#37CC3D',
