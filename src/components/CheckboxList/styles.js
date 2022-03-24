53fa92a207f79c646380b87ab939b8a9ebf390b2
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-03-24 15:11:08 -0300

    feat: add checkbox list component

---
+++ src/components/CheckboxList/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const CheckboxListWrapper = styled.div`
+  margin-top: 20px;
+`;
+
+export const Boxes = styled.ul`
+  display: grid;
+  grid-template-columns: 50% 50%;
+  align-items: center;
+  margin-top: 5px;
+  
+  @media (max-width: 500px) {
+    grid-template-columns: 100%;
+  }
+`;
+
+export default CheckboxListWrapper;
