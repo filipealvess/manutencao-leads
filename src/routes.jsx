e05d553ed99be167115b36009e6b102207f7076d
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-03-24 15:11:48 -0300

    feat: add new lead page

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
+import NewLead from './pages/NewLead';
 
 export default function AppRoutes() {
   return (
@@ -9,6 +10,7 @@ export default function AppRoutes() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/novo-lead" element={<NewLead />} />
       </Routes>
     </BrowserRouter>
   );
