0d89c755633d703b2616920a320b498cb6f39b7c
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-03-28 09:57:58 -0300

    feat: add checked property and handleInputChange function

--- src/components/Checkbox/index.jsx
+++ src/components/Checkbox/index.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CheckboxWrapper, { Input } from './styles';
 
-export default function Checkbox({ option, onChange }) {
+export default function Checkbox({ option, onChange, checked }) {
+  const [isChecked, setIsChecked] = useState(false);
+
+  useEffect(() => {
+    if (checked !== null) {
+      setIsChecked(checked);
+    }
+  }, [checked]);
+
+  function handleInputChange() {
+    const input = { checked: !isChecked, value: option };
+
+    setIsChecked(!isChecked);
+    onChange({ target: input });
+  }
+
   return (
     <CheckboxWrapper>
-      <Input type="checkbox" value={option} onChange={onChange} />
+      <Input
+        type="checkbox"
+        value={option}
+        onChange={handleInputChange}
+        checked={isChecked}
+      />
       {option}
     </CheckboxWrapper>
   );
